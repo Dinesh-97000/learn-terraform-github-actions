aa0876b76072bb2b0985b4c3d0d854d66ea5d02c
Dinesh-97000 <129315014+Dinesh-97000@users.noreply.github.com>
2023-03-29 16:51:18 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "Din-Project"
 
     workspaces {
       name = "learn-terraform-github-actions"
